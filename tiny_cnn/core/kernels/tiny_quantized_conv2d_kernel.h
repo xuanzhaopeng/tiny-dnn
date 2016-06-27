16cf57cc57b07a3436939798024e7ac87b4d35b5
Wangyida <wangyida123@outlook.com>
2016-06-27 11:14:25 +0800

    quantization utilities and tests

--- tiny_cnn/core/kernels/tiny_quantized_conv2d_kernel.h
+++ tiny_cnn/core/kernels/tiny_quantized_conv2d_kernel.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2013, Taiga Nomi
+    Copyright (c) 2016, Taiga Nomi, Edgar Riba
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -24,35 +24,16 @@
     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#define _CRT_SECURE_NO_WARNINGS
-#include "picotest/picotest.h"
-#include "tiny_cnn/tiny_cnn.h"
+#pragma once
 
-using namespace tiny_cnn::activation;
+#include "tiny_cnn/core/params/conv_params.h"
+#include "tiny_cnn/core/kernels/tiny_quantization_kernel.h"
 
-#include "test_network.h"
-#include "test_average_pooling_layer.h"
-#include "test_dropout_layer.h"
-#include "test_max_pooling_layer.h"
-#include "test_fully_connected_layer.h"
-#include "test_deconvolutional_layer.h"
-#include "test_convolutional_layer.h"
-#include "test_target_cost.h"
-#include "test_large_thread_count.h"
-#include "test_lrn_layer.h"
-#include "test_batch_norm_layer.h"
-#include "test_nodes.h"
-#include "test_core.h"
-#include "test_models.h"
-#include "test_slice_layer.h"
-#include "test_power_layer.h"
-#include "test_quantization.h"
-
-#ifdef CNN_USE_CAFFE_CONVERTER
-#include "test_caffe_converter.h"
-#endif
+namespace tiny_cnn {
+namespace core {
+namespace kernels {
 
 
-int main(void) {
-    return RUN_ALL_TESTS();
-}
+}  // namespace kernels
+}  // namespace core
+}  // namespace tiny_cnn
